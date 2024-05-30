52175698639a5b2cf5f0af443eaa9b06e3534a7c
Senpaihere1 <167081748+Senpaihere1@users.noreply.github.com>
2024-05-30 18:25:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/crunchyrollanimeinhindidubb")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Team_Shadow_Group")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/nexusworldgc")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/nexusworldgc")
 # groupz-------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
